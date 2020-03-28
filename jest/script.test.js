928ba0f620ec345f0990455ba23f8a3fb2eed8b6
DanielJWagener <daniel.j.wagener@gmail.com>
2020-03-28 10:53:32 -0400

    Add more tests

--- jest/script.test.js
+++ jest/script.test.js
@@ -1,8 +1,19 @@
 const googleSearch = require("./script.js");
 
-dbMock = ["dog.com", "cheesepuff.com", "dogpics.com"];
+dbMock = ["dog.com", "cheesepuff.com", "dogpics.com", "lol.com", "nose.come"];
 
-it("is searching Google", () => {
-  expect(googleSearch("dog", dbMock)).toEqual(["dog.com", "dogpics.com"]);
-  expect(googleSearch("asdfasdf", dbMock)).toEqual([]);
+describe("googleSearch", () => {
+  it("is searching Google", () => {
+    expect(googleSearch("dog", dbMock)).toEqual(["dog.com", "dogpics.com"]);
+    expect(googleSearch("asdfasdf", dbMock)).toEqual([]);
+  });
+
+  it("works with undefined and null input", () => {
+    expect(googleSearch(undefined, dbMock)).toEqual([]);
+    expect(googleSearch(null, dbMock)).toEqual([]);
+  });
+
+  it("does not return more than three matches", () => {
+    expect(googleSearch(".com", dbMock).length).toEqual(3);
+  });
 });
