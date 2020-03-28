21b336221d5f0f3db0e68cbc3281246e9adfb60a
DanielJWagener <daniel.j.wagener@gmail.com>
2020-03-28 11:39:39 -0400

    Write tests with mocks and spies

--- jest/script2.test.js
+++ jest/script2.test.js
@@ -1,10 +1,13 @@
 const fetch = require("node-fetch");
 const swapi = require("./script2");
 
+const expectedCount = 87;
+const minExpectedLength = 5;
+
 it("calls swapi to get people", done => {
   expect.assertions(1);
   swapi.getPeople(fetch).then(data => {
-    expect(data.count).toEqual(87);
+    expect(data.count).toEqual(expectedCount);
     done();
   });
 });
@@ -12,7 +15,27 @@ it("calls swapi to get people", done => {
 it("calls swapi to get people with a promise", () => {
   expect.assertions(2);
   return swapi.getPeoplePromise(fetch).then(data => {
-    expect(data.count).toEqual(87);
-    expect(data.results.length).toBeGreaterThan(5);
+    expect(data.count).toEqual(expectedCount);
+    expect(data.results.length).toBeGreaterThan(minExpectedLength);
+  });
+});
+
+it("has getPeople return count and results", () => {
+  const mockFetch = jest.fn().mockReturnValue(
+    Promise.resolve({
+      json: () =>
+        Promise.resolve({
+          count: expectedCount,
+          results: [0, 1, 2, 3, 4, 5, 7]
+        })
+    })
+  );
+
+  expect.assertions(4);
+  return swapi.getPeoplePromise(mockFetch).then(data => {
+    expect(mockFetch.mock.calls.length).toBe(1);
+    expect(mockFetch).toBeCalledWith("https://swapi.co/api/people");
+    expect(data.count).toEqual(expectedCount);
+    expect(data.results.length).toBeGreaterThan(minExpectedLength);
   });
 });
